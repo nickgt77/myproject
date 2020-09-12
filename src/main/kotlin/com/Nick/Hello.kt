030748784b741ef58b2639abfca7ea5b0240152c
Nickgt LI <gp0ax0@gmail.com>
2020-09-12 15:36:25 +0800

    Initial commit

---
+++ src/main/kotlin/com/Nick/Hello.kt
@@ -0,0 +1,13 @@
+package com.Nick
+
+fun main() {
+//     println("Hello World")
+//     Human().hello()
+       val h = Human()
+     h.hello()
+}
+class Human{
+     fun hello(){
+          println("Hello World")
+     }
+}
